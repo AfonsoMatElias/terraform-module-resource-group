03e0ed9d87b0babbcacc9ecefa62391d8e24a87a
Marcus Martins Leite <mvm.leite@gmail.com>
2025-10-05 22:35:01 -0300

    Adjust outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,2 @@
 output "id"       { value = azurerm_resource_group.resource_group.id }
 output "name"     { value = azurerm_resource_group.resource_group.name }
-output "location" { value = azurerm_resource_group.resource_group.location }
