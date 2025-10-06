a06174b67a2c914c742c780894300372a6b14547
Marcus Martins Leite <mvm.leite@gmail.com>
2025-10-05 21:02:02 -0300

    Adjust my files

--- terragrunt.tf
+++ terragrunt.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-storage"
-    storage_account_name = "saterraform1754"
-    container_name       = "terraform"
-    key                  = "terraform-module-resource-group/terraform.tfstate"
+  backend "azurerm" {}
   }
-}
+
   
